c5ab16ee93044824e13f678fa49655d97f933d02
AmanSingh25 <aman.singh98162@gmail.com>
2022-09-09 10:28:14 -0500

    adds UI to the react app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <div className="fi-calculator">
+        <div className="display">
+      </div>
+
+        <div className="fi-operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>del</button>
+        </div>
+
+        <div className="fi-digits">
+          { createNums() }
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
